679c4dc3f68820c6ea5de68efff3a9c3d2ae724d
Arthur DELANDRE <arthur@macbook-pro-de-arthur.home>
2018-10-29 15:53:00 +0100

    Grille

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void dessiner_tab(int tab);
+void dessiner_tab(int tab[3][3]);
+char joueur(int k);
 
 int main(void) {
-	int tab[3][3] = {{0, 0, 0},{0, 0, 0},{0, 0, 0}};
+	printf("TicTacToe\n");
+	int tab[3][3] = {{rand() % 3, rand() % 3, rand() % 3},
+					{rand() % 3, rand() % 3, rand() % 3},
+					{rand() % 3, rand() % 3, rand() % 3}};
 	dessiner_tab(tab);
 	return EXIT_SUCCESS;
 }
 
-void dessiner_tab(int tab) {
+void dessiner_tab(int tab[3][3]) {
 	// Dessine le plateau passé en paramètre
+	char ligne[3] = {'A', 'B', 'C'};
+	printf("  1 2 3\n");
 	for (int i = 0; i < 3; ++i)
 	{
-		printf("\t %d %d %d\n", tab[i][0], tab[i][1], tab[i][2]);
+		printf("%c %c %c %c\n",ligne[i], joueur(tab[i][0]), joueur(tab[i][1]), joueur(tab[i][2]));
 
+	}
+}
+
+char joueur(int k) {
+	if (k == 1)
+	{
+		return 'X';
+	}else{
+		if (k == 2)
+		{
+			return 'O';
+		}else{
+			return '.';
+		}
 	}
 }
