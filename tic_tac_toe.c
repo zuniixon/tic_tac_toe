ef7f4324b6172f34ec23711c45b8a87507f5c897
Arthur DELANDRE <arthur@macbook-pro-de-arthur.home>
2018-10-29 14:06:30 +0100

    Création des fonctions exigées

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+	Ébauche tableau :
+
+	int tableau[3][3] = {{0, 0, 0},
+						 {0, 0, 0},
+						 {0, 0, 0}}
+*/
+int tab_init();
+void dessiner_tab(int tab, int longueur);
+
 int main(void) {
 
 	return EXIT_SUCCESS;
+}
+
+int tab_init() {
+	int tab[3][3] = {{0, 0, 0},
+						 {0, 0, 0},
+						 {0, 0, 0}};
+	return tab
+}
+
+void dessiner_tab(int tab, int longueur) {
+
 }
